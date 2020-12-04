4357487b847e381dd556951285e72668155dd5b8
vula <laanhvu99@gmail.com>
2020-12-04 13:37:34 +0700

    OK

--- Test01/hapi/api/Account/detectImage/Module.js
+++ Test01/hapi/api/Account/detectImage/Module.js
@@ -2,23 +2,25 @@ const puppeteer = require('puppeteer');
 const axios = require('axios').default;
 const fs = require('fs');
 const ResCode = require('project/constants/ResponseCode')
+const path = require('path');
+const _= require('lodash');
 const FormData = require('form-data');
 
 
-const PROXY_SERVER = {
-    IP: '139.162.24.32',
-    PORT: 9999,
-};
+// const PROXY_SERVER = {
+//     IP: '139.162.24.32',
+//     PORT: 9999,
+// };
 
 // const PROXY_SERVER = {
 //     IP: '31.220.33.13',
 //     PORT: 1212,
 // };
 
-// const PROXY_SERVER = {
-//     IP: '185.236.202.135',
-//     PORT: 3128,
-// };
+const PROXY_SERVER = {
+    IP: '185.236.202.135',
+    PORT: 3128,
+};
 // const PROXY_SERVER = {
 //     IP: '102.129.133.60',
 //     PORT: 3128,
@@ -44,8 +46,9 @@ downloadImage = async (url, dirSave, nameIMG) => {
         if (url.match(/\.(jpeg|jpg|gif|png)$/) === null) {
             return false;
         }
-        const filePath = `${dirSave}/${nameIMG}.jpg`
-        const writer = fs.createWriteStream(filePath)
+        const filePath = `${path.join(dirSave, nameIMG)}.jpg`
+        //const filePath = 'ANHVU.jpeg'
+        const writer = fs.createWriteStream(filePath,{flags : 'w'})
         const response = await axios({
             url,
             method: 'GET',
@@ -53,16 +56,20 @@ downloadImage = async (url, dirSave, nameIMG) => {
         })
         response.data.pipe(writer)
         return new Promise((resolve, reject) => {
-            writer.on('finish', resolve(filePath))
+            writer.on('finish', () => {
+                console.log('Tải Ảnh Thành Công');
+                resolve(filePath);
+                //resolve(response)
+            })
             writer.on('error', reject)
         })
     } catch (error) {
         //console.log(error);
     }
 }
+
 detectIMG = async (url) => {
     try {
-        let dirSave = './private/upload';
         const browser = await puppeteer.launch({
             headless: true,
             // args: [
@@ -74,7 +81,7 @@ detectIMG = async (url) => {
             return page.evaluate(async () => {
                 return await new Promise(resolve => {
                     grecaptcha.ready(async function () {
-                        const token = await grecaptcha.execute('6Lc1vJQUAAAAAGktFIQ-6hbHTVQXHWF174WTMjDE', {action: 'vision'});
+                        const token = await grecaptcha.execute('6Lc1vJQUAAAAAGktFIQ-6hbHTVQXHWF174WTMjDE', { action: 'vision' });
                         resolve(token);
                     });
                 })
@@ -84,35 +91,27 @@ detectIMG = async (url) => {
 
         await browser.close();
 
-        const nameFolder = new Date().toLocaleDateString().split('/').join('.');
-        const nameIMG = `ID${new Date().getTime()}`
-        if (!fs.existsSync(`${dirSave}/${nameFolder}`)) {
-            fs.mkdirSync(`${dirSave}/${nameFolder}/`)
-        }
-        dirSave = dirSave + '/' + nameFolder;
-        let filePath = undefined;
+        const formData = new FormData();
+        let imageURL = undefined;
         try {
-            filePath = await downloadImage(url, dirSave, nameIMG)
-        } catch (e) {
-            return {data: {errorMessage: 'Tải Hình Thất Bại - Thử Lại Sau'}}
+             imageURL = await axios({
+                url,
+                method: 'GET',
+                responseType: 'stream'
+            })
+            formData.append('token',token);
+            formData.append('image',imageURL.data);
+            console.log({ formData });
+        }catch (e) {
+            console.log( {error : e});
+            return { data: { errorMessage: 'Không thể lấy hình ảnh từ URL' } };
         }
-        if (!filePath) {
-            return {
-                data: {errorMessage: 'Url không phải là hình ảnh'}
-            };
-        }
-
-        const form = new FormData();
-        form.append('token', token);
-        // console.log({ filePath });
-        form.append('image', fs.createReadStream(filePath));
-        console.log( { form })
         try {
-            const response = await axios({
+            const getDataFormFPTAI = await axios({
                 url: URL_REQUEST,
                 method: 'POST',
-                headers: form.getHeaders(),
-                data: form,
+                headers: formData.getHeaders(),
+                data: formData,
                 proxy: {
                     //protocol: 'https',
                     // auth: {
@@ -122,31 +121,120 @@ detectIMG = async (url) => {
                     host: PROXY_SERVER.IP,
                     port: PROXY_SERVER.PORT,
                 },
-                timeout: 60 * 60,
-               
+                timeout: 5 * 1000,
             })
-            if (response.data.errorCode !== 0) {
-                fs.unlinkSync(filePath)
+            if (getDataFormFPTAI.data.errorCode === 0) {
+                let dirSave = './private/upload/';
+                const nameFolder = new Date().toLocaleDateString().split('/').join('.');
+                const nameIMG = `ID${new Date().getTime()}`
+                if (!fs.existsSync(dirSave)) {
+                    fs.mkdir(dirSave, (err) => err);
+                }
+                dirSave = path.join(dirSave, nameFolder);
+                if (!fs.existsSync(dirSave)) {
+                    fs.mkdir(dirSave, (err) => err)
+                }
+                const write = fs.createWriteStream(path.join(dirSave,nameIMG));
+                imageURL.data.pipe(write)
+                    .on('error',() => {console.log('Tải ảnh thất bại')});
             }
-            return response;
+            return getDataFormFPTAI;
         } catch (error) {
             //return fake data if request to FPT AI fail
-            fs.unlinkSync(filePath)
+            console.log({ error })
+            //fs.unlinkSync(filePath)
+            if (error.response && error.response.status === 429) {
+                return { data: { errorMessage: 'Quá Số Lần Gửi Yêu Cầu' } };
+            }
             return {
-                data: {errorMessage: 'Không Thể Kết Nối Đến Proxy Server'}
+                data: { errorMessage: 'Không Thể Kết Nối Đến Proxy Server' }
             };
         }
+
+
+        // const nameFolder = new Date().toLocaleDateString().split('/').join('.');
+        // const nameIMG = `ID${new Date().getTime()}`
+        // if (!fs.existsSync(dirSave)) {
+        //     fs.mkdir(dirSave, (err) => err);
+        // }
+        // dirSave = path.join(dirSave, nameFolder);
+        // if (!fs.existsSync(dirSave)) {
+        //     fs.mkdir(dirSave, (err) => err)
+        // }
+        // // dirSave = path.join(dirSave,nameFolder);
+        //
+        // let filePath = undefined;
+        // try {
+        //     filePath = await downloadImage(url, dirSave, nameIMG)
+        // } catch (e) {
+        //     return { data: { errorMessage: 'Tải Hình Thất Bại - Thử Lại Sau' } }
+        // }
+        // if (!filePath) {
+        //     return {
+        //         data: { errorMessage: 'Url không phải là hình ảnh' }
+        //     };
+        // }
+        // //const Test = fs.createReadStream(__dirname);
+        // const Test2 = fs.createReadStream(filePath)
+        //     .on('error',() => {
+        //         return { data: { errorMessage: 'Lỗi hình ảnh' } };
+        //     });
+        //
+        // const form = new FormData();
+        // form.append('token', token);
+        // // console.log({ filePath });
+        // //form.append('image', fs.createReadStream(filePath));
+        // form.append('image', Test2);
+        //
+        // //form.append('image',fs.createReadStream(path.join(__dirname,'test.jpg')))
+        // console.log({ form })
+        // try {
+        //     const response = await axios({
+        //         url: URL_REQUEST,
+        //         method: 'POST',
+        //         headers: form.getHeaders(),
+        //         data: form,
+        //         // proxy: {
+        //         //     //protocol: 'https',
+        //         //     // auth: {
+        //         //     //     username: '192.168.05.10',
+        //         //     //     password: '@123456789@'
+        //         //     // },
+        //         //     host: PROXY_SERVER.IP,
+        //         //     port: PROXY_SERVER.PORT,
+        //         // },
+        //         // timeout: 60 * 60,
+        //
+        //     })
+        //     if (response.data.errorCode !== 0) {
+        //         fs.unlinkSync(filePath)
+        //     }
+        //     return response;
+        // } catch (error) {
+        //     //return fake data if request to FPT AI fail
+        //     console.log({ error })
+        //     //fs.unlinkSync(filePath)
+        //     if (error.response.status === 429) {
+        //         return { data: { errorMessage: 'Quá Số Lần Gửi Yêu Cầu' } };
+        //     }
+        //     return {
+        //         data: { errorMessage: 'Không Thể Kết Nối Đến Proxy Server' }
+        //     };
+        // }
     } catch (error) {
         console.log(error);
+        return {
+            data: { errorMessage: 'Lỗi không xác định - Thử lại sau' }
+        };
     }
 }
 
 module.exports = async (request, reply) => {
-    const {url} = request.payload;
+    const { url } = request.payload;
     const response = await detectIMG(url);
     //console.log({data: response.data})
     if (response.data.errorCode !== 0) {
-        return reply.api({message: response.data.errorMessage}).code(ResCode.REQUEST_FAIL);
+        return reply.api({ message: response.data.errorMessage }).code(ResCode.REQUEST_FAIL);
     }
-    return reply.api({data: response.data}).code(ResCode.REQUEST_SUCCESS);
+    return reply.api({ data: response.data }).code(ResCode.REQUEST_SUCCESS);
 };
--- Test01/hapi/api/Account/detectImage/Route.js
+++ Test01/hapi/api/Account/detectImage/Route.js
@@ -1,29 +1,29 @@
-const Module = require('./Module');
-const Joi = require('mecore').Joi;
-const ResponseCode = require('project/constants/ResponseCode')
+    const Module = require('./Module');
+    const Joi = require('mecore').Joi;
+    const ResponseCode = require('project/constants/ResponseCode')
 
-module.exports = [{
-    method: 'POST',
-    path: '/api/detect-image',
-    handler: Module,
-    options: {
-        description: "Phân giải ảnh",
-        auth: false,
-        validate: {
-            payload: Joi.object({
-                url: Joi.string().required().example('https://s3.storage.2banh.vn/image/2016/03/khi-di-lam-cmnd-12-so-phai-mac-ao-co-co-nhe-moi-nguoi-luu-y-dieu-nay-27-1459216122-56f9defa3766f.jpg'),
-            })
-        },
-        response: {
-            status: {
-                [ResponseCode.REQUEST_SUCCESS]: Joi.object({
-                    data: Joi.object()
-                }).description('Thành công'),
-                [ResponseCode.REQUEST_FAIL]: Joi.object({
-                    message : Joi.string().example('Lý Do Thất Bại'),
-                }).description('Thất bại')
+    module.exports = [{
+        method: 'POST',
+        path: '/api/detect-image',
+        handler: Module,
+        options: {
+            description: "Phân giải ảnh",
+            auth: false,
+            validate: {
+                payload: Joi.object({
+                    url: Joi.string().required().example('https://s3.storage.2banh.vn/image/2016/03/khi-di-lam-cmnd-12-so-phai-mac-ao-co-co-nhe-moi-nguoi-luu-y-dieu-nay-27-1459216122-56f9defa3766f.jpg'),
+                })
             },
-        },
-        tags: ['api', 'Login']
-    }
-}]
+            response: {
+                status: {
+                    [ResponseCode.REQUEST_SUCCESS]: Joi.object({
+                        data: Joi.object()
+                    }).description('Thành công'),
+                    [ResponseCode.REQUEST_FAIL]: Joi.object({
+                        message : Joi.string().example('Lý Do Thất Bại'),
+                    }).description('Thất bại')
+                },
+            },
+            tags: ['api', 'Login']
+        }
+    }]
